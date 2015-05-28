59d83940ec4ef8cdf0c0eaa991b8ee86a3bd27bf
瘦的销魂 <tut.bear@gmail.com>
2015-05-28 10:28:42 +0800

    修改首页

--- wp-content/themes/tutear/index.php
+++ wp-content/themes/tutear/index.php
@@ -96,7 +96,7 @@
                     <?php endwhile; ?> 
                 </ul>
             </div>
-            <div class="pan">
+            <div class="pan" style="margin-left:10px">
                 <?php if (get_option('home_catid2')) { $catid2 = get_option('home_catid2'); } ?>
                 <h3 class="pan-header"><?php wp_list_categories('include='.$catid2.'&title_li=&style=none'); ?></h3>
                 <ul class="pan-body">
@@ -109,33 +109,39 @@
             </div>
             <div class="clear"></div>
             <div class="pan middle">
-                <h3 class="pan-header">孵化案例</h3>
+                <h3 class="pan-header"><a hre="#">孵化项目</a></h3>
                 <ul class="pan-body">
-                    <li>
-                        <a href="#">
-                            <img src="" />
-                            <p>不知名茶</p>
+                    <li class="first">
+                        <a class="img-wrap" href="#">
+                            <img src="wp-content/themes/tutear/images/shili.jpg" />
                         </a>
+                        <a href="">不知名茶</a>
                     </li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-            </div>
-    
-
-            <div class="pan">
-                <?php if (get_option('home_catid3')) { $catid3 = get_option('home_catid3'); } ?>
-                <h3 class="pan-header"><?php wp_list_categories('include='.$catid3.'&title_li=&style=none'); ?></h3>
-                <ul class="pan-body">
-                    <?php query_posts('cat='.$catid3.'&showposts=8'); ?>
-                    <?php while (have_posts()) : the_post(); ?>
                     <li>
-                    <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></li>     
-                    <?php endwhile; ?> 
+                        <a class="img-wrap"  href="#">
+                            <img src="wp-content/themes/tutear/images/shili.jpg" />
+                        </a>
+                        <a href="">不知名茶</a>
+                    </li>
+                    <li>
+                        <a class="img-wrap" href="#">
+                            <img src="wp-content/themes/tutear/images/shili.jpg" />
+                        </a>
+                        <a href="">不知名茶</a>
+                    </li>
+                    <li>
+                        <a class="img-wrap" href="#">
+                            <img src="wp-content/themes/tutear/images/shili.jpg" />
+                            
+                        </a>
+                        <a href="">不知名茶</a>
+                    </li>
                 </ul>
+
             </div>
-            <div class="pan">
+            <div class="clear"></div>
+
+            <div class="pan tuijian" style="width:100%">
                 <?php if (get_option('home_catid4')) { $catid4 = get_option('home_catid4'); } ?>
                 <h3 class="pan-header"><?php wp_list_categories('include='.$catid4.'&title_li=&style=none'); ?></h3>
                 <ul class="pan-body">
@@ -146,6 +152,7 @@
                     <?php endwhile; ?> 
                 </ul>
             </div>
+            <div class="clear"></div>
         </div>
         <div class="sider">
             <div class="pan video">
@@ -157,6 +164,7 @@
 
                 </div>
             </div>
+            <div class="clear"></div>
             <div class="nums">
                 <ul class="nums-list">
                     <li><a class="btn0" href=""></a><p>章程</p></li>
@@ -168,15 +176,90 @@
                 </ul>
 
             </div>
+            <div class="clear"></div>
+
+             <div class="member">
+                <h3 class="member-header"><a hre="#">会员单位</a></h3>
+                <ul class="member-list">
+                    <li><a href=""><img src="wp-content/themes/tutear/images/164851114.jpg" alt=""></a></li>
+                    <li><a href=""><img src="wp-content/themes/tutear/images/164851114.jpg" alt=""></a></li>
+                    <li><a href=""><img src="wp-content/themes/tutear/images/164851114.jpg" alt=""></a></li>
+                    <li><a href=""><img src="wp-content/themes/tutear/images/164851114.jpg" alt=""></a></li>
+                    <li><a href=""><img src="wp-content/themes/tutear/images/164851114.jpg" alt=""></a></li>
+                    <li><a href=""><img src="wp-content/themes/tutear/images/164851114.jpg" alt=""></a></li>
+                    <li><a href=""><img src="wp-content/themes/tutear/images/164851114.jpg" alt=""></a></li>
+                </ul>
+
+            </div>
 
         </div>
 
-        <div class="fd-link">
-            
-        </div>
+                    <div class="pan foot-link">
+                <h3 class="pan-header"><a hre="#">友情链接</a></h3>
+                <table width="100%" border="0" cellspacing="0" cellpadding="0" style="margin-top:10px;">
+       
+        <tbody><tr>
+          <td width="10%" height="37" valign="top"><strong>合作媒体:</strong></td>
+          <td width="90%" valign="top"><a href="http://www.prnasia.com/" target="_blank" title="美通社">美通社</a> |
+<a href="http://www.xinhuanet.com/" target="_blank" title="新华网">新华网</a> |
+<a href="http://www.nbd.com.cn/" target="_blank" title="每日经济新闻">每日经济新闻</a> |
+<a href="http://www.jrj.com.cn/" target="_blank" title="金融界">金融界</a> |
+<a href="http://www.hexun.com/" target="_blank" title="和讯网">和讯网</a> |
+<a href="http://www.pedaily.cn/" target="_blank" title="投资界">投资界</a> |
+<a href="http://www.zero2ipogroup.com" target="_blank" title="清科集团">清科集团</a> |
+<a href="http://www.chinaventure.com.cn/" target="_blank" title="投资中国">投资中国</a> |
+<a href="http://www.xjr365.cn/" target="_blank" title="新金融">新金融</a> |
+<a href="http://www.ChinaValue.net/" target="_blank" title="价值中国">价值中国</a> |
+<a href="http://www.moneydao.com/" target="_blank" title="投资有道">投资有道</a> |
+<a href="http://www.topcapital.com.cn/pages/index.asp" target="_blank" title="投资与合作">投资与合作</a> |
+<a href="http://www.xincaijing.com/Index.html" target="_blank" title="新财经">新财经</a> |
+<a href="http://www.investorchina.com.cn/" target="_blank" title="投资者报">投资者报</a> |
+<a href="http://www.sino-manager.com/" target="_blank" title="经理人网">经理人网</a> |
+<a href="http://www.qianjing.cn/Index.html" target="_blank" title="钱经">钱经</a> |
+<a href="http://www.capital-markets.cn/" target="_blank" title="资本市场">资本市场</a> |
+<a href="http://www.capitalweek.com.cn/index.html" target="_blank" title="证券市场周刊">证券市场周刊</a> |
+<a href="http://www.simuwang.com/" target="_blank" title="私募排排网">私募排排网</a> |
+<a href="http://content.businessvalue.com.cn/" target="_blank" title="商业价值">商业价值</a> |
+<a href="Link-1.html"><font color="#FF0000">更多</font></a></td>
+        </tr>
+        
+        
+        <tr>
+          <td height="37" valign="top"><strong>国内组织:</strong></td>
+          <td height="37" valign="top"><a href="http://www.zgc.gov.cn/" target="_blank" title="中关村国家自主创新示范区">中关村国家自主创新示范区</a> |
+<a href="http://www.zhongguancun.com.cn/" target="_blank" title="中关村科技园区海淀园管理委员会">中关村科技园区海淀园管理委员会</a> |
+<a href="http://www.zpark.com.cn/" target="_blank" title="中关村软件园">中关村软件园</a> |
+<a href="http://www.bjmx-online.com" target="_blank" title="中关村科技企业家协会">中关村科技企业家协会</a> |
+<a href="http://www.gqx.org.cn/" target="_blank" title="北京中关村高新技术企业协会">北京中关村高新技术企业协会</a> |
+<a href="http://www.bjtf.cn/" target="_blank" title="北京市科技金融促进会">北京市科技金融促进会</a> |
+<a href="http://www.bjit.org.cn" target="_blank" title="北京信息化协会">北京信息化协会</a> |
+<a href="www.lnvcpea.org" target="_blank" title="辽宁省股权和创业投资协会">辽宁省股权和创业投资协会</a> |
+<a href="http://www.zhongchou.cn/open" target="_blank" title="众筹网">众筹网</a> |
+<a href="http://www.tusstar.com/" target="_blank" title="启迪创业孵化器">启迪创业孵化器</a> |
+<a href="Link-2.html"><font color="#FF0000">更多</font></a></td>
+        </tr>
+        
+        
+        
+        <tr>
+          <td height="37" valign="top"><strong>国际组织:</strong></td>
+          <td height="37" valign="top"><a href="http://www.insme.org" target="_blank" title="国际中小企业协会">国际中小企业协会</a> |
+<a href="http://www.wbaa.biz/" target="_blank" title="世界天使投资协会">世界天使投资协会</a> |
+<a href="http://www.nvca.org" target="_blank" title="美国风险投资协会">美国风险投资协会</a> |
+<a href="http://www.iva.co.il" target="_blank" title="以色列风险投资协会">以色列风险投资协会</a> |
+<a href="http://www.angelcapitalassociation.org" target="_blank" title="天使资本协会">天使资本协会</a> |
+<a href="http://www.nacocanada.com/Home.aspx" target="_blank" title="加拿大国家天使资本协会">加拿大国家天使资本协会</a> |
+<a href="http://www.cvca.ca" target="_blank" title="加拿大风险投资协会">加拿大风险投资协会</a> |
+<a href="http://www.nasvf.org" target="_blank" title="国家种子基金和风险协会">国家种子基金和风险协会</a> |
+<a href="http://www.eban.org" target="_blank" title="欧洲商务协会">欧洲商务协会</a> |
+<a href="http://www.melekyatirimcilardernegi.org" target="_blank" title="土耳其天使投资协会">土耳其天使投资协会</a> |
+<a href="Link-3.html"><font color="#FF0000">更多</font></a></td>
+        </tr>
+      </tbody></table>
+            </div>
         <div class="clear"></div>
     </div>
-
+    
     <div class="wrap footer"> <?php if ( get_option('com_copyright') ) { echo stripslashes(get_option('com_copyright'));} ?></div>
 
     <script>
@@ -190,8 +273,6 @@
             hoverPause: true
         });
     });
-
-
     </script>
 
 </body>
